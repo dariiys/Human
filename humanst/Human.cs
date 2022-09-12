2b1df83e062e7417113d092f4d4f95dcd42eacf6
dariiys <gavrilenkod2111@gmail.com>
2022-09-12 17:11:03 +0300

    Human

---
+++ humanst/Human.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @class
+{
+    enum Nation { Ukranian, Polish, Italian};
+    class Human
+    {
+        protected string _name;
+        protected string _surname;
+        protected int _age;
+        protected int _height;
+        protected bool _habbits;
+        protected Nation _nation;
+        protected Adress _adress;
+
+
+        public Human()
+        {
+            Console.WriteLine("Створення людини");
+            this._name = "Daria";
+            this._surname = "Havrylenko";
+            this._age = 18;
+            this._height = 156;
+            this._habbits = false;
+            this._nation = Nation.Ukranian;
+            this._adress = new Adress();
+        }
+        public Human(string name, string surname, int age, int height, bool habbits, Nation nation, Adress adress)
+        {
+            this._name = name;
+            this._surname = surname;
+            this._age = age;
+            this._height = height;
+            this._habbits = habbits;
+            this._nation = nation;
+            this._adress = adress;
+        }
+        public static Human operator +(Human one, Human two)
+        {
+            Human result = new Human();
+            result._age = one._age + two._age;
+            result._habbits = one._habbits && two._habbits;
+            return result;
+        }
+        public static bool operator >(Human one, Human two)
+        {
+            bool result = one._age > two._age;
+            return result;
+        }
+        public static bool operator <(Human one, Human two)
+        {
+            bool result = one._age < two._age;
+            return result;
+        }
+        public virtual void printInfo()
+        {
+            string data =
+                "Name: " + this._name + "\n" +
+                "Surname: " + this._surname + "\n" +
+                "Age: " + this._age.ToString() + "\n" +
+                "Height: " + this._height.ToString() + "\n" +
+                "Is Habbits: " + this._habbits.ToString() + "\n" +
+                "Nation: " + this._nation.ToString() + "\n" +
+                "Adress: " + this._adress.toString();
+            Console.WriteLine(data);
+        }
+        public string toStr()
+        {
+            string str;
+            str = "Name: " + this._name + "\n" +
+                "Surname: " + this._surname + "\n" +
+                "Age: " + this._age.ToString() + "\n" +
+                "Height: " + this._height.ToString() + "\n" +
+                "Is Habbits: " + this._habbits.ToString() + "\n" +
+                "Nation: " + this._nation.ToString() + "\n" +
+                "Adress: " + this._adress.toString();
+            return str;
+        }
+        public static void inputInfo(listHuman list)
+        {
+            Console.WriteLine("Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Surname: ");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Age: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Height: ");
+           int height = int.Parse(Console.ReadLine());
+            Console.WriteLine("Habbits: ");
+            bool habbits = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Nation: ");
+            Nation nation = (Nation)Enum.Parse(typeof(Nation), Console.ReadLine(), true);
+            Adress adr = new Adress();
+            Human n = new Human(name, surname, age, height, habbits, nation, adr.inputadress());
+            list.add(n);
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Surname
+        {
+            get { return _surname; }
+            set { _surname = value; }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        public int Height
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
+
+
+        public bool Habbits
+        {
+            get { return _habbits; }
+            set { _habbits = value; }
+        }
+
+        public Nation Nation
+        {
+            get { return _nation; }
+            set { _nation = value; }
+        }
+
+        public Adress Adress
+        {
+            get { return _adress; }
+            set { _adress = value; }
+        }
+
+    }
+}
---
+++ humanst/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace @class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Human> listHuman = new List<Human>();
+          
+            listHuman list = new listHuman();
+            Human.inputInfo(list);
+            list.show();
+            string choice = null;
+            while (choice != "0")
+            {
+                Console.WriteLine("Выберите действие ");
+                Console.WriteLine("0 - выйти с программы.");
+                Console.WriteLine("1 - распечатать все объекты");
+                Console.WriteLine("2 - найти объект по имени");
+                Console.WriteLine("3 - сортировка объектов по возрасту и вывести информацию");
+                Console.WriteLine("4 - сортировка объектов по фамилии и вывести информацию");
+                Console.WriteLine("5 - создать новый объект и добавить в список");
+                Console.WriteLine("6 - изменить значения поля объекта");
+                Console.WriteLine("7 - удалить объект");
+                choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "0":
+                        Console.WriteLine("До свидания!");
+                        break;
+                    case "1":
+                        list.show();
+                        break;
+                    case "2":
+                        Console.WriteLine("Введите имя ");
+                        string name = Console.ReadLine();
+                        list.findName(name);
+                        break;
+                    case "3":
+                        list.sort_age();
+                        break;
+                    case "4":
+                        list.sort_surname();
+                        break;
+                    case "5":
+                        Human.inputInfo(list);
+                        Console.WriteLine("Теперь наш список выглядит так: ");
+                        list.show();
+                        break;
+                    case "6":
+                        string c = null;
+                        while (c != "0")
+                        {
+                            Console.WriteLine("Выберите действие ");
+                            Console.WriteLine("0 - вернуться в главное меню.");
+                            Console.WriteLine("1 - изменить имя");
+                            Console.WriteLine("2 - изменить фамилию");
+                            Console.WriteLine("3 - изменить возраст");
+                            c = Console.ReadLine();
+                            switch (c)
+                            {
+                                case "0":
+                                    break;
+                                case "1":
+                                    Console.WriteLine("Введите имя объекта которое хотите изменить: ");
+                                    string firstName = Console.ReadLine();
+                                    Console.WriteLine("Введите имя на которое хотите изменить: ");
+                                    string secondName = Console.ReadLine();
+                                    list.find_change_name(firstName, secondName);
+                                    break;
+                                case "2":
+                                    Console.WriteLine("Введите фамилию объекта которую хотите изменить: ");
+                                    string firstSurname = Console.ReadLine();
+                                    Console.WriteLine("Введите фамилию на которую хотите изменить: ");
+                                    string secondSurname = Console.ReadLine();
+                                    list.find_change_surname(firstSurname, secondSurname);
+                                    break;
+                                case "3":
+                                    Console.WriteLine("Введите имя объекта чей возраст вы хотите изменить: ");
+                                    string Name = Console.ReadLine();
+                                    Console.WriteLine("Введите фамилию объекта чей возраст вы хотите изменить: ");
+                                    string SurName = Console.ReadLine();
+                                    Console.WriteLine("Введите возраст на который хотите изменить: ");
+                                    int Age = int.Parse(Console.ReadLine());
+                                    list.find_change_age(Name, SurName, Age);
+                                    break;
+                                default:
+                                    Console.WriteLine("Вы ввели неверный номер!");
+                                    break;
+                            }
+                        }
+                        break;
+                    case "7":
+                        Console.WriteLine("Введите имя объекта который хотите удалить: ");
+                        string name_human = Console.ReadLine();
+                        Console.WriteLine("Введите фамилию объекта который хотите удалить: ");
+                        string surName = Console.ReadLine();
+                        list.remove(name_human, surName);
+                        break;
+
+                    default:
+                        Console.WriteLine("Вы ввели неверный номер!");
+                        break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
